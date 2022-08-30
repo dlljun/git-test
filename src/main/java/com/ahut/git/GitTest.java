4be681d228e8c6895d1dcc6b40395fd4cb50ce33
Tom <tom.ahut.com>
2022-08-30 09:27:55 +0800

    first test

---
+++ src/main/java/com/ahut/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ahut.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-29 21:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好来得及！！！");
+    }
+}
