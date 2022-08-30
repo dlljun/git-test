34fa59e25f8642aacb44ba778301d58143ef43a7
Tom <tom.ahut.com>
2022-08-30 09:44:08 +0800

    hot-fix test

--- src/main/java/com/ahut/git/GitTest.java
+++ src/main/java/com/ahut/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好来得及！！！");
         System.out.println("最新消息");
+        System.out.println("hot-fix");
     }
 }
